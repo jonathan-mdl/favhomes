4fb4363352c7ee1dfd59a1eaa409a14a3823bbe1
Jonathan <jmedel.7@gmail.com>
2020-06-05 17:06:33 -0400

    vista home con foto y autor

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from posts.models import Post
 # Create your views here.
+
+class PostList(ListView):
+    model = Post
